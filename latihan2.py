1a00ef3af5f5a1c2f9c8c374bad2d31b1ea03d9d
Sultan Rafif <sultanrafif65@gmail.com>
2019-10-27 14:19:01 +0700

    file pertama saya

---
+++ latihan2.py
@@ -0,0 +1,16 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=input("masukan nilai a:")
+masukan nilai a:25
+>>> b=input("masukan nilai b:")
+masukan nilai b:5
+>>> print("hasil penggabungan {1}&{0}=%d" .format(25,5) %(25+5))
+hasil penggabungan 5&25=30
+>>> #Konversi nilai variable
+>>> a=int(25)
+>>> b=int(5)
+>>> print("hasil penjumlahan {1}+{0}=%d" .format(25,5) %(25+5))
+hasil penjumlahan 5+25=30
+>>> print("hasil pembagian {1}/{0}=%d" .format(25,5) %(25/5))
+hasil pembagian 5/25=5
+>>> 
